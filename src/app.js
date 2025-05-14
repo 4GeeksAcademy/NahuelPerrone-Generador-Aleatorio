f2a6005ac4b283e1e96e00596e1ca8a2b69f4f94
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-05-14 19:37:18 +0000

    fin del trabajo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+ for(let i=0; i < pronoun.length; i ++){
+  for(let x=0; x < adj.length; x ++){
+    for(let p=0; p < noun.length; p ++){
+      let Dominio = pronoun[i]+adj[x]+noun[p]+ ".com";
+      console.log(Dominio);
+    }
+  }
+}
